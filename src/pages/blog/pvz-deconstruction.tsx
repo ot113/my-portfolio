6b4d51fc72618538ba48a4e56f9605f93b06edb0
ot113 <158476901+ot113@users.noreply.github.com>
2026-05-16 01:39:16 +0300

    Delete src/pages/blog/pvz-deconstruction.tsx

--- src/pages/blog/pvz-deconstruction.tsx
+++
@@ -1,9 +0,0 @@
-export default function PvZDeconstruction() {
-  return (
-    <iframe
-      src="/pvz-deconstruction.html"
-      style={{ width: "100vw", height: "100vh", border: 0 }}
-      title="PvZ Deconstruction"
-    />
-  );
-}
