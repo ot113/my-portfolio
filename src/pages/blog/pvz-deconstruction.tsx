f4528bcbfce168ab8b1fad8797a422626a075aed
ot113 <158476901+ot113@users.noreply.github.com>
2026-05-16 01:45:34 +0300

    Create pvz-deconstruction.tsx

---
+++ src/pages/blog/pvz-deconstruction.tsx
@@ -0,0 +1,13 @@
+import { ReactNode } from "react";
+
+export default function PvZDeconstruction() {
+  return (
+    <iframe
+      src="/pvz-deconstruction.html"
+      style={{ width: "100vw", height: "100vh", border: 0 }}
+      title="PvZ Deconstruction"
+    />
+  );
+}
+
+PvZDeconstruction.getLayout = (page: ReactNode) => page;
