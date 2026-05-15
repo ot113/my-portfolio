7ffe3151db603f61df4a38be7a3077edc4989ea8
ot113 <158476901+ot113@users.noreply.github.com>
2026-05-16 01:31:16 +0300

    Create pvz-deconstruction.tsx

---
+++ src/pages/blog/pvz-deconstruction.tsx
@@ -0,0 +1,9 @@
+export default function PvZDeconstruction() {
+  return (
+    <iframe
+      src="/pvz-deconstruction.html"
+      style={{ width: "100vw", height: "100vh", border: 0 }}
+      title="PvZ Deconstruction"
+    />
+  );
+}
